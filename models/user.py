6d467e48f093aab0d7f2f57e00dd8d47b37b4b84
nasergamal <naser.gamal98@gmail.com>
2023-10-02 15:38:33 +0300

    apis

--- models/user.py
+++ models/user.py
@@ -3,6 +3,7 @@
 import models
 from models.base_model import BaseModel, Base
 from os import getenv
+import hashlib
 import sqlalchemy
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
@@ -27,3 +28,5 @@ class User(BaseModel, Base):
     def __init__(self, *args, **kwargs):
         """initializes user"""
         super().__init__(*args, **kwargs)
+        if self.password:
+            self.password = hashlib.md5(self.password.encode()).hexdigest()
